60f374d48447b20ba1d1530b136d0b3666cf749f
Bayu <104434265+briyantoe@users.noreply.github.com>
2026-03-05 01:43:23 +0700

    Add dark mode toggle feature
    Implement dark mode toggle functionality with local storage support.

---
+++ script.js
@@ -0,0 +1,14 @@
+const toggle = document.getElementById("darkToggle")
+
+toggle.onclick = () => {
+document.body.classList.toggle("dark")
+
+localStorage.setItem(
+"theme",
+document.body.classList.contains("dark") ? "dark":"light"
+)
+}
+
+if(localStorage.getItem("theme") === "dark"){
+document.body.classList.add("dark")
+}
